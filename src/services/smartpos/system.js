71a16817a2b5d66dcb5003da0fa2dc9c76cd18fe
Davismcintyre5 <147067472+Davismcintyre5@users.noreply.github.com>
2026-06-09 08:07:15 +0300

    Refactor system API responses to return full data

--- src/services/smartpos/system.js
+++ src/services/smartpos/system.js
@@ -2,10 +2,20 @@ import api from './api';
 
 export async function getSystem() {
   const res = await api.get('/system');
-  return res.data.data; // { settings }
+  return res.data;
 }
 
 export async function updateSystem(data) {
   const res = await api.put('/system', data);
-  return res.data.data;
+  return res.data;
+}
+
+export async function enableMaintenance({ reason, durationHours, message }) {
+  const res = await api.post('/system/maintenance/enable', { reason, durationHours, message });
+  return res.data;
+}
+
+export async function disableMaintenance({ sendCompletionEmail = true }) {
+  const res = await api.post('/system/maintenance/disable', { sendCompletionEmail });
+  return res.data;
 }
